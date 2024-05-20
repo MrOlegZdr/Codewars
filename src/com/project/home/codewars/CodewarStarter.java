fc7c128763fd23e57a8c35cae1cdbccb4c8027c0
MrOleg <mrdrozdilla@gmail.com>
2024-05-20 17:25:30 +0200

    Refactor main()

--- src/com/project/home/codewars/CodewarStarter.java
+++ src/com/project/home/codewars/CodewarStarter.java
@@ -8,17 +8,33 @@ public class CodewarStarter {
 
 	public static void main(String[] args) {
 
+		// Run findSpaceship
+		checkFindSpaceship();
+
+		// Run checkSocks
+		checkSocks();
+
+		// Run arrayDiff
+
+	}
+
+	private static void checkFindSpaceship() {
+
 		String resultShip = EightiesKids2.findSpaceship("...\n...\n..X\n...\n...\n...");
 		System.out.println(resultShip);
 
+	}
+
+	private static void checkSocks() {
+
 		String[] resultSocksPunky = EightiesKids2.getSocks("Punky", new String[] { "red", "blue", "blue", "green" });
 		String[] resultSocksHenry = EightiesKids2.getSocks("Henry", new String[] { "red", "blue", "blue", "green" });
-		String[] resultSocks = EightiesKids2.getSocks("Punky", new String[] { "red", "red", "blue"});
-		
+		String[] resultSocks = EightiesKids2.getSocks("Punky", new String[] { "red", "red", "blue" });
+
 		System.out.println(Arrays.toString(resultSocksPunky));
 		System.out.println(Arrays.toString(resultSocksHenry));
 		System.out.println(Arrays.toString(resultSocks));
-		
+
 	}
 
 }
